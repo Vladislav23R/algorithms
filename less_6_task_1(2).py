dd67dff5d01cc0c87519ca96c31bf074050c829d
Vladislav Raev <streletz00@gmail.com>
2021-01-03 23:46:48 +0300

    completed task

---
+++ less_6_task_1(2).py
@@ -0,0 +1,35 @@
+# Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125,…
+# Количество элементов (n) вводится с клавиатуры.
+
+# Выделяемая память - 556 байт
+
+from sys import getsizeof
+
+
+def func_sum_bytes(variables):
+    sum_bytes = 0
+    for variable in variables:
+        if isinstance(variable, list):
+            for item in variable:
+                sum_bytes += getsizeof(item)
+        sum_bytes += getsizeof(variable)
+    return sum_bytes
+
+
+n = 10
+a = 1
+result = 0
+b = []
+for i in range(n):
+    b.append(a)
+    a *= (-0.5)
+    if i == n - 1:
+        for j in b:
+            result += j
+
+
+variables = [n, a, result, b, i, j]
+
+c = func_sum_bytes(variables)
+print(f'Количество выделяемой памяти - {c} байт')
+print(result)
